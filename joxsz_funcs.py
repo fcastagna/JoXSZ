a295c9f69ea79c387f586ca8182ff6f151abb2dd
Fabio Castagna <fabio.castagna@inaf.it>
2020-06-17 17:40:27 +0200

    update Z_defPars

--- joxsz_funcs.py
+++ joxsz_funcs.py
@@ -244,7 +244,7 @@ def add_param_unit():
     mb.ParamGaussian.__init__ = pargau_new_init
     mb.ParamGaussian.__repr__ = pargau_new_repr
 
-def Z_defpars(self):
+def Z_defPars(self):
     '''
     Update defpars function of metallicity object to include unit measure
     ---------------------------------------------------------------------
